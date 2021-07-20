2d57ec0fa905fd2601c851e3e738dbd39bba78a4
Vladimirsky <vladimir.bervin@gmail.com>
2021-07-20 23:45:31 +0300

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCjssGENYbZ9wKShp1mKSqn0QDSwbpIdQ0',
+  fbDbUrl: 'https://angular-blog-63f24-default-rtdb.europe-west1.firebasedatabase.app'
+};
